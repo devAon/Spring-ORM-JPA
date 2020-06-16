9bc4e5bb845e3f736a7b4488fdf79d497dcf9e89
GeniusK <geniusk1047@naver.com>
2020-06-17 01:58:28 +0900

    정렬 수정~

--- src/main/java/hello/Main.java
+++ src/main/java/hello/Main.java
@@ -3,13 +3,11 @@ package hello;
 /**
  * Created by qwone4@gmail.com on 2020-06-16
  * Blog : http://aonee.tistory.com
-        * Github : http://github.com/devAon
-        */
+ * Github : http://github.com/devAon
+ */
 public class Main {
     public static void main(String args[]){
         System.out.println("hello");
-
-
     }
 }
 
